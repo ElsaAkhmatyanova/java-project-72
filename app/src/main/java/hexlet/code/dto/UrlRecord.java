467554d15e6cbdae023f7cd95e5a4c375331716f
ElsaAkhmatyanova <akh24071998@yandex.ru>
2025-08-25 11:55:01 +0500

    format createdAt

--- app/src/main/java/hexlet/code/dto/UrlRecord.java
+++ app/src/main/java/hexlet/code/dto/UrlRecord.java
@@ -1,9 +1,7 @@
 package hexlet.code.dto;
 
-import java.time.LocalDate;
-
 public record UrlRecord(long id,
                         String name,
-                        LocalDate lastCheck,
+                        String lastCheck,
                         int code) {
 }
--- app/src/main/java/hexlet/code/mapper/UrlsMapper.java
+++ app/src/main/java/hexlet/code/mapper/UrlsMapper.java
@@ -3,12 +3,16 @@ package hexlet.code.mapper;
 import hexlet.code.dto.UrlRecord;
 import hexlet.code.model.Urls;
 
+import java.time.format.DateTimeFormatter;
+
 public class UrlsMapper {
+
+    private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
 
     public static UrlRecord mapToRecord(Urls urlsEntity) {
         return new UrlRecord(urlsEntity.getId(),
                 urlsEntity.getName(),
-                urlsEntity.getCreatedAt().toLocalDate(),
+                urlsEntity.getCreatedAt() != null ? DATE_TIME_FORMATTER.format(urlsEntity.getCreatedAt()) : "",
                 200);
     }
 }
